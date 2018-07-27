f08f99aab9ba473fb4403ade4879bf9deae35d3b
jtoledo3970 <jtoledo3970@gmail.com>
2018-07-27 13:15:43 -0700

    Initial Commit

---
+++ LatLong Converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LatLong Converter
+//
+//  Created by Administrator on 7/27/18.
+//  Copyright © 2018 Toledo's IT Solutions, Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
